8b6986cbb0a988acb2af2246ac6a8428463cf878
LambdaLe <iswangli@foxmail.com>
2023-03-19 16:00:15 +0800

    chore: 引入框架代码

---
+++ src/Springboot/staticanalyzer/src/main/java/com/staticanalyzer/staticanalyzer/StaticAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.staticanalyzer.staticanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaticAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaticAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/Springboot/staticanalyzer/src/test/java/com/staticanalyzer/staticanalyzer/StaticAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.staticanalyzer.staticanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaticAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
